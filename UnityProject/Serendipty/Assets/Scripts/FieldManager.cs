2ceba2dec331699c7d719d76513ed8f0932be419
foba1 <foba1@naver.com>
2022-11-18 22:04:23 +0900

    Implement function to update field color.
    Implement function to update field color.

--- UnityProject/Serendipty/Assets/Scripts/FieldManager.cs
+++ UnityProject/Serendipty/Assets/Scripts/FieldManager.cs
@@ -27,6 +27,42 @@ public class FieldManager : MonoBehaviour
         DontDestroyOnLoad(this);
     }
 
+    private void UpdateFieldColor()
+    {
+        if (selectedFieldIndex == -1)
+        {
+            for (int i = 0; i < fieldObject.Length; i++)
+            {
+                fieldObject[i].GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < fieldObject.Length; i++)
+            {
+                if (i / 6 != selectedFieldIndex / 6)
+                {
+                    if (fieldObject[i].transform.childCount > 0)
+                    {
+                        fieldObject[i].GetComponent<SpriteRenderer>().color = new Color(1f, 160f / 255f, 160f / 255f, 1f);
+                    }
+                    else
+                    {
+                        fieldObject[i].GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+                    }
+                }
+                else if (i == selectedFieldIndex)
+                {
+                    fieldObject[i].GetComponent<SpriteRenderer>().color = new Color(199f / 255f, 1f, 170f / 255f, 1f);
+                }
+                else
+                {
+                    fieldObject[i].GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+                }
+            }
+        }
+    }
+
     public void SelectField(int fieldIndex)
     {
         if (fieldIndex < 0 || fieldIndex >= fieldObject.Length) return;
@@ -44,12 +80,10 @@ public class FieldManager : MonoBehaviour
                 if (selectedFieldIndex / 6 == fieldIndex / 6)
                 {
                     Move(selectedFieldIndex, fieldIndex);
-                    selectedFieldIndex = -1;
                 }
                 else
                 {
                     AttackManager.Instance.Attack(selectedFieldIndex, fieldIndex);
-                    selectedFieldIndex = -1;
                 }
             }
             else
@@ -57,14 +91,11 @@ public class FieldManager : MonoBehaviour
                 if (selectedFieldIndex / 6 == fieldIndex / 6)
                 {
                     Move(selectedFieldIndex, fieldIndex);
-                    selectedFieldIndex = -1;
-                }
-                else
-                {
-                    selectedFieldIndex = -1;
                 }
             }
+            selectedFieldIndex = -1;
         }
+        UpdateFieldColor();
     }
 
     public void Move(int fieldIndex1, int fieldIndex2)
