c85d595142ef1855ff7321481668a2ffe1672c8b
foba1 <foba1@naver.com>
2022-11-24 22:22:39 +0900

    Fix animation error.
    Fix animation error.

--- UnityProject/Serendipty/Assets/Scripts/Card/Light/HolyKnight.cs
+++ UnityProject/Serendipty/Assets/Scripts/Card/Light/HolyKnight.cs
@@ -39,14 +39,13 @@ public class HolyKnight : Creature
 
     IEnumerator AttackCoroutine(int pos)
     {
-        float xOffset = 15f;
         if (curPosition / 6 == 0)
         {
-            transform.position = FieldManager.Instance.fieldObject[pos].transform.position + new Vector3(-xOffset, 0f, 0f);
+            transform.position = FieldManager.Instance.fieldObject[pos].transform.position + new Vector3(-15f, 0f, 0f);
         }
         else
         {
-            transform.position = FieldManager.Instance.fieldObject[pos].transform.position + new Vector3(xOffset, 0f, 0f);
+            transform.position = FieldManager.Instance.fieldObject[pos].transform.position + new Vector3(10f, 0f, 0f);
         }
 
         Animator animator = transform.GetChild(0).GetComponent<Animator>();
@@ -87,7 +86,14 @@ public class HolyKnight : Creature
         health -= damage;
         if (health > 0)
         {
-            health += armor;
+            if (damage >= 20)
+            {
+                health += armor;
+            }
+            else
+            {
+                health += damage;
+            }
             UpdateInfoText();
             StartCoroutine(GetDamagedCoroutine());
         }
