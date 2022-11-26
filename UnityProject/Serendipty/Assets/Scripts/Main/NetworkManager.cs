e546c1a5bb427640987b9f88d1f368da36e9fe2a
JeongTaeWoong <72208120+JeongTaeWoong@users.noreply.github.com>
2022-11-27 02:07:36 +0900

    KingOfSea 임시

--- UnityProject/Serendipty/Assets/Scripts/Main/NetworkManager.cs
+++ UnityProject/Serendipty/Assets/Scripts/Main/NetworkManager.cs
@@ -41,7 +41,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     {
         if (PhotonNetwork.IsConnected)
         {
-            StaticVariable.MyDeck = "33000000333030003330";
+            StaticVariable.MyDeck = "00000000000000922220";
             if (StaticVariable.MyDeck == "" || StaticVariable.MyDeck.Length != StaticVariable.CardCount)
             {
                 connectStateText.text = "Please select your deck.";
