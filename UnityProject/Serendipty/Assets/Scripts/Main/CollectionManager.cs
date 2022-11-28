05e95e342a877e592e453fa5736974f48cbd842d
foba1 <foba1@naver.com>
2022-11-28 23:46:53 +0900

    Implement button pressed.
    Implement button pressed.

--- UnityProject/Serendipty/Assets/Scripts/Main/CollectionManager.cs
+++ UnityProject/Serendipty/Assets/Scripts/Main/CollectionManager.cs
@@ -39,6 +39,16 @@ public class CollectionManager : MonoBehaviour
         UpdateInfo();
     }
 
+    public void ButtonDown(GameObject button)
+    {
+        button.transform.GetChild(0).transform.localPosition -= new Vector3(0f, 20f, 0f);
+    }
+
+    public void ButtonUp(GameObject button)
+    {
+        button.transform.GetChild(0).transform.localPosition += new Vector3(0f, 20f, 0f);
+    }
+
     public void Main()
     {
         SceneManager.LoadScene("Main");
